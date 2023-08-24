f2fe4f0e88f1220b878330c8f12cdd9ac97d3973
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-08-24 12:00:07 +0300

    Completed Task

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+list=["hello", "goodbye"]
+class Anagram:
+    def __init__(self, word):
+        self.word =word
+
+    
+    def match(self, words):
+        lower_self_word = self.word.lower()
+        return [word for word in words if sorted(word.lower()) == sorted(lower_self_word)and word.lower() != lower_self_word]
+
